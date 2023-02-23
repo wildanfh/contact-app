4de4176824198aa8dfde4603760f6df24964ef67
wildanf <wildanf2005@gmail.com>
2023-02-23 21:44:43 +0700

    initial commit

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: 'Dimas Saputra',
+      tag: 'dimasmds',
+      imageUrl: '/images/dimasmds.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Arif Faizin',
+      tag: 'arifaizin',
+      imageUrl: '/images/arifaizin.jpeg',
+    },
+    {
+      id: 3,
+      name: 'Rahmat Fajri',
+      tag: 'rfajri27',
+      imageUrl: '/images/rfajri27.jpeg',
+    },
+    {
+      id: 4,
+      name: 'Hudza Santoso',
+      tag: 'hudzantoso8',
+      imageUrl: '/images/hudza.jpg',
+    },
+  ];
+ }
+  
+ export { getData };
